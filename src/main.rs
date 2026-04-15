b43300e6941dede7e1287b76c2ff5ec63b72a1e2
Blair Xu <masterystacker@gmail.com>
2026-04-15 07:41:40 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ use std::net::TcpListener;
 fn main() {
 
     let listener =TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming(){
+        match stream{
+            Ok(stream) =>{
+                
+            }
+            Err(e) =>println!("error: {}",e),
+        }
+    }
     
    
 }
